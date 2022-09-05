2e1234ac8ae2f8d3abd6253eb3ba1e4f49a72f2a
yuyumi <changyu2000@gmail.com>
2022-09-05 05:42:03 -0400

    Tutorial Code Finished

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
+
 //connection to db
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-console.log("Connected to db!");
-app.listen(3000, () => console.log("Server Up and running"));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, err => {
+    if(err) throw err;
+    console.log("Connected to db!");
+    app.listen(3120, () => console.log("Server Up and running"));
 });
 
 // view engine configuration
@@ -40,3 +42,29 @@ app.post('/',async (req, res) => {
         res.redirect("/");
     }
 });
+
+//UPDATE
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndRemove(id, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
