0e87f756873cf834d735a7c6da9d5e137b1f4655
yuyumi <changyu2000@gmail.com>
2022-09-08 19:47:12 -0400

    Heroku files

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, err => {
     if(err) throw err;
     console.log("Connected to db!");
-    app.listen(3120, () => console.log("Server Up and running"));
+    app.listen(process.env.PORT || 3120, () => console.log("Server Up and running"));
 });
 
 // view engine configuration
